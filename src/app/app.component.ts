75b9acaf69ac1177eddfb4337b1d33bfaee6cfb6
Paradox92 <pschubert1992@gmx.de>
2017-08-23 17:57:57 +0200

    Starting Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
+
 export class AppComponent {
-  title = 'my First Angular App';
+    title = 'Tour of Heroes';
+    hero = 'Windstorm';
 }
