79c71129526e5789ef690dbc86adae04e2f21346
Paradox92 <pschubert1992@gmx.de>
2017-08-27 11:28:30 +0200

    Sixth Tutorial Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,22 +14,24 @@ import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule
-  ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-  ],
-  providers: [ HeroService ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        HeroSearchComponent
+    ],
+    providers: [HeroService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
